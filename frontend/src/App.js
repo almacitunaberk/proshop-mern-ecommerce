9ff278bf895d24ea793c9cada7081fa198fcc92d
Tunaberk Almaci <almacitunaberk@gmail.com>
2021-08-10 14:05:35 +0300

    Routers are added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,22 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
           <Container>
-            <HomeScreen />
+            <Route exact path="/" component={HomeScreen} />
+            <Route path="/product/:id" component={ProductScreen} />
           </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
