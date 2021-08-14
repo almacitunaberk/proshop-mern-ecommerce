c057e6082fae5ff8576d01d9ed9db5c2c9b96beb
Tunaberk Almaci <almacitunaberk@gmail.com>
2021-08-14 16:59:34 +0300

    Authentication endpoint started

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
-import colors from "colors";
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import colors from 'colors';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 // ROUTES
-import productRoutes from "./routes/productRoutes.js";
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -13,11 +14,14 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.send('API is running')
+  res.send('API is running');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 
@@ -26,5 +30,3 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
-
-
