1a481274640492f43cadce97dc59b1b15da4c1b0
Tunaberk Almaci <almacitunaberk@gmail.com>
2021-08-18 21:55:13 +0300

    Order Screen added

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -1,11 +1,18 @@
-import { ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_CREATE_FAIL } from '../constants/orderConstants';
+import {
+  ORDER_CREATE_REQUEST,
+  ORDER_CREATE_SUCCESS,
+  ORDER_CREATE_FAIL,
+  ORDER_DETAILS_REQUEST,
+  ORDER_DETAILS_FAIL,
+  ORDER_DETAILS_SUCCESS,
+} from '../constants/orderConstants';
 
 export const orderCreateReducer = (state = {}, action) => {
   const { type, payload } = action;
 
   switch (type) {
     case ORDER_CREATE_REQUEST:
-      return { loading: false };
+      return { loading: true };
     case ORDER_CREATE_SUCCESS:
       return {
         loading: false,
@@ -22,14 +29,14 @@ export const orderCreateReducer = (state = {}, action) => {
   }
 };
 
-export const orderDetailsReducer = (state = { orderItems: [], shippingAddress: {} }, action) => {
+export const orderDetailsReducer = (state = { loading: true, orderItems: [], shippingAddress: {} }, action) => {
   const { type, payload } = action;
 
   switch (type) {
     case ORDER_DETAILS_REQUEST:
       return {
         ...state,
-        loading: false,
+        loading: true,
       };
     case ORDER_DETAILS_SUCCESS:
       return {
