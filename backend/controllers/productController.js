9ecbe28a8c1412809facfbe68fe4fb1fb8b184c5
Tunaberk Almaci <almacitunaberk@gmail.com>
2021-08-21 23:36:32 +0300

    Product reviews backend side finished

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -79,4 +79,37 @@ const updateProductById = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProductById, getProducts, deleteProductById, createProduct, updateProductById };
+// @route   POST /api/products/:id/review
+// @desc    Comment on a product
+// @acess   Private
+const reviewOnProduct = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body;
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    const alreadyReviewed = product.reviews.find((r) => r.user.toString() === req.user._id.toString());
+    if (alreadyReviewed) {
+      res.status(400);
+      throw new Error('Product already reviewed');
+    }
+    const review = {
+      name: req.user.name,
+      user: req.user._id,
+      comment: comment,
+      rating: Number(rating),
+    };
+
+    product.reviews.push(review);
+
+    product.numReviews = product.reviews.length;
+
+    product.rating = product.reviews.reduce((acc, item) => item.rating + acc, 0) / product.reviews.length;
+
+    await product.save();
+
+    res.status(201).json({ message: 'Review added' });
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
+export { getProductById, getProducts, deleteProductById, createProduct, updateProductById, reviewOnProduct };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,6 +6,7 @@ import {
   deleteProductById,
   createProduct,
   updateProductById,
+  reviewOnProduct,
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
@@ -16,5 +17,7 @@ router
   .get(getProductById)
   .delete(protect, admin, deleteProductById)
   .put(protect, admin, updateProductById);
+
+router.route('/:id/reviews').post(protect, reviewOnProduct);
 
 export default router;
