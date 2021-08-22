7e29f6969144556f9b3511e3310fcd5e9b38ce95
Tunaberk Almaci <almacitunaberk@gmail.com>
2021-08-22 12:21:08 +0300

    Meta data added and the project is finished

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
 import { getListProducts } from '../actions/productActions';
@@ -7,6 +8,7 @@ import Loader from '../components/Loader';
 import Message from '../components/Message';
 import Paginate from '../components/Paginate';
 import ProductCarousel from '../components/ProductCarousel';
+import Meta from '../components/Meta';
 
 const HomeScreen = ({ match }) => {
   const dispatch = useDispatch();
@@ -24,7 +26,19 @@ const HomeScreen = ({ match }) => {
 
   return (
     <>
-      {!keyword && <ProductCarousel />}
+      <Meta
+        title="Welcome to Proshop"
+        description="We sell the best products for cheap"
+        keywords="Electronics, buy electronics, cheap"
+      />
+
+      {!keyword ? (
+        <ProductCarousel />
+      ) : (
+        <Link to="/" className="btn btn-light">
+          Home
+        </Link>
+      )}
       <h1>Latest Products</h1>
       {loading ? (
         <Loader />
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -6,6 +6,7 @@ import Rating from '../components/Rating';
 import { getProductDetails, reviewOnProduct } from '../actions/productActions';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
+import Meta from '../components/Meta';
 import { PRODUCT_REVIEW_RESET } from '../constants/productConstants';
 
 const ProductScreen = ({ history, match }) => {
@@ -54,6 +55,7 @@ const ProductScreen = ({ history, match }) => {
         <Message variant="danger">{error}</Message>
       ) : (
         <>
+          <Meta title={product.name} />
           <Row>
             <Col md={6}>
               <Image src={product.image} alt={product.name} fluid />
